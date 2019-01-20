4e2c60691c8cfe0d81d8667bfb9891e15f133a4e
Ian Tse <ian.c.tse@gmail.com>
2019-01-20 01:07:44 -0800

    Added metadata to setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='apollo',
+      version='0.1',
+      author='Ian Tse',
+      author_email='ian.c.tse@gmail.com',
+      url='https://github.com/ianctse/apollo',
+      license='MIT')
